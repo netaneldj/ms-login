7e359909269aef3ea5ebb8ec0ff46c43ce69a9d3
Netanel Jamilis <njamilis@fi.uba.ar>
2023-11-28 09:46:05 -0300

    handle argument not valid exception

--- login/src/main/java/com/jamilis/login/exception/GlobalExceptionHandler.java
+++ login/src/main/java/com/jamilis/login/exception/GlobalExceptionHandler.java
@@ -1,13 +1,11 @@
 package com.jamilis.login.exception;
 
-import java.io.UnsupportedEncodingException;
-import java.security.GeneralSecurityException;
-import java.security.NoSuchAlgorithmException;
 import java.time.Instant;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -18,6 +16,13 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(UserAlreadyExistException.class)
     public ResponseEntity<Object> handleUserAlreadyExistException(Exception e) {
         ErrorMessage errorMessage = new ErrorMessage(Instant.now().toString(), HttpStatus.BAD_REQUEST.value(), e.getMessage());
+        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleArgumentNotValidException(MethodArgumentNotValidException  e) {
+        ErrorMessage errorMessage = new ErrorMessage(Instant.now().toString(), HttpStatus.BAD_REQUEST.value(),
+                e.getBindingResult().getFieldError().getDefaultMessage());
         return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
     }
 
